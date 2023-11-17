6a5a7fcc46caa22aaf036710c4a05411c280ab8a
CODE76 <144792396+CODE76-POS@users.noreply.github.com>
2023-11-17 18:12:29 +0300

    Delete js directory

--- js/TWoBj7knyxm5.js
+++
@@ -1,9 +0,0 @@
-var hldr = document.querySelectorAll('.fr');
-
-hldr.forEach(el =>{
-        el.addEventListener('mouseenter', () =>  {
-        hldr.forEach(fr=>fr.classList.remove('active'));
-        el.classList.add('active');
-        })
-     });
-
